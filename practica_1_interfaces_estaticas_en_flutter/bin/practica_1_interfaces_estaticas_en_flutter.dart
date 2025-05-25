234e9a532519930a17b664cd71dbcc2d5e4294a4
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-24 23:12:55 -0500

    1 commit

--- practica_1_interfaces_estaticas_en_flutter/bin/practica_1_interfaces_estaticas_en_flutter.dart
+++
@@ -1,5 +0,0 @@
-import 'package:practica_1_interfaces_estaticas_en_flutter/practica_1_interfaces_estaticas_en_flutter.dart' as practica_1_interfaces_estaticas_en_flutter;
-
-void main(List<String> arguments) {
-  print('Hello world: ${practica_1_interfaces_estaticas_en_flutter.calculate()}!');
-}
--- practica_1_interfaces_estaticas_en_flutter/test/practica_1_interfaces_estaticas_en_flutter_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practica_1_interfaces_estaticas_en_flutter/practica_1_interfaces_estaticas_en_flutter.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
